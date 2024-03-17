17acdd6171e405ea4b3dd57f1ff1a67ab3621d90
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-03-17 13:26:49 +0300

    steam gamescope experimentation (unsuccessful. bummer!!)

--- modules/software/distractions/steam.nix
+++ modules/software/distractions/steam.nix
@@ -16,6 +16,14 @@ in {
       enableRenice = true;
       settings = {};
     };
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    programs.steam.gamescopeSession = {
+      enable = true;
+	    args = [ "-W 1600" "-H 900" "-r 60" "--expose-wayland" "-e" ];
+    };
     user.packages = [ pkgs.protontricks pkgs.steam-run ];
   };
 }

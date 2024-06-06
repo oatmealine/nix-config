263acdc6c772fc2780aeae557cec749821cd5fd7
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-06-06 15:46:30 +0300

    fix syncthing tray, partly

--- modules/software/system/syncthing.nix
+++ modules/software/system/syncthing.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 let
@@ -11,7 +11,15 @@ in {
   config = mkIf cfg.enable {
     # declare sync folders & devices here
     # edit: turns out you can't with hm. huh
-    hm.services.syncthing.enable = true;
-    hm.services.syncthing.tray.enable = true;
+    hm.services.syncthing = {
+      enable = true;
+      tray = {
+        enable = true;
+        package = pkgs.syncthingtray-minimal;
+        command = "syncthingtray --wait";
+        #package = pkgs.qsyncthingtray;
+        #command = "qsyncthingtray --wait";
+      };
+    };
   };
 }

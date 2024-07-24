fea6627d93d891dab0ea4d7e9f2131e6c97972de
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-07-25 00:13:12 +0300

    mess around w fish

--- modules/software/system/fish.nix
+++ modules/software/system/fish.nix
@@ -7,19 +7,25 @@ let
 in {
   options.modules.software.system.fish = {
     enable = mkEnableOption "Enable fish, the friendly interpreted shell";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.unstable.fish;
+    };
   };
 
   config = mkIf cfg.enable {
     user.packages = [ pkgs.grc ];
 
-    environment.systemPackages = with pkgs.fishPlugins; [ fzf-fish tide ];
+    environment.systemPackages = with pkgs.fishPlugins; [ fzf-fish tide done ];
 
-    users.defaultUserShell = pkgs.fish;
+    users.defaultUserShell = cfg.package;
     programs.fish.enable = true;
+    programs.fish.package = cfg.package;
     hm.programs.fish = let
       colorScript = nix-colors-lib.shellThemeFromScheme { scheme = config.colorScheme; };
     in {
       enable = true;
+      package = cfg.package;
       plugins = [ { name = "grc"; src = pkgs.fishPlugins.grc.src; } ];
       interactiveShellInit = ''
         sh ${colorScript}

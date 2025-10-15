d4f78345eea964b3c749fcd33380e2f56645a3bc
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2025-10-15 11:00:49 +0300

    let's try to remember to commit the changes this time

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
+notify-send 'rebuild.sh requires auth'
 sudo nix-env -p /nix/var/nix/profiles/system --set $deriv
 sudo $deriv/bin/switch-to-configuration switch

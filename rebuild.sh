6b667d8bfb8b58678b58da6ba7a20fdfc008cce4
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2025-11-08 04:21:36 +0300

    nh is pretty cool actually

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
-notify-send 'rebuild.sh requires auth'
-sudo nix-env -p /nix/var/nix/profiles/system --set $deriv
-sudo $deriv/bin/switch-to-configuration switch
+#deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
+#notify-send 'rebuild.sh requires auth'
+#sudo nix-env -p /nix/var/nix/profiles/system --set $deriv
+#sudo $deriv/bin/switch-to-configuration switch
+
+NIXPKGS_ALLOW_UNFREE=1 nh os switch . -- --impure

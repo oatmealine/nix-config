e451ea83b265844886469598a7d9b2a29c813808
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2025-11-08 04:22:27 +0300

    casual-pre-loader 0.0.0 > 1.6.6

--- packages/casual-pre-loader/default.nix
+++ packages/casual-pre-loader/default.nix
@@ -4,28 +4,44 @@
 }:
 
 with pkgs;
+with pkgs.python3Packages;
 
 let
+  valve-parsers = buildPythonPackage rec {
+    pname = "valve-parsers";
+    version = "1.0.7";
+    src = fetchPypi {
+      pname = "valve_parsers";
+      inherit version;
+      sha256 = "sha256-/3hV1+niFs7YrpI+xtTbN5Zc/87jgryMWb6ruG+Hnmo=";
+    };
+    format = "pyproject";
+    propagatedBuildInputs = [
+      setuptools
+    ];
+  };
   pythonPath = python3.withPackages (
     ps:
     with ps;
-    [ pyqt6 pillow pyxdg ]
+    [ pyqt6 valve-parsers requests packaging platformdirs ]
   );
 in
 stdenv.mkDerivation rec {
   pname = "casual-pre-loader";
-  version = "0.0.0";
+  version = "1.6.6";
 
   src = fetchFromGitHub {
     owner = "cueki";
     repo = "casual-pre-loader";
-    rev = "234c95c7cb2783dcfa66fb850bce7c58d77bed65";
-    hash = "sha256-8XuSdK+qwksfbE5nXkBqhM0PGKY1o6l2ZiBLiz1HxSw=";
+    rev = "11993ea06f07be6b3345094bfdd2565a0fb7db1f";
+    hash = "sha256-DIt8Z6DatBwIIfQ+48BRCboF30SuQaSp89x1mO1GJio=";
   };
 
   nativeBuildInputs = [
     pkgs.makeWrapper
   ];
+
+  dontCheckForBrokenSymlinks = true;
 
   installPhase = ''
     mkdir -p $out

66e426a9e038f98250da1b4b46f72ca9d134514a
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2025-12-04 00:30:58 +0300

    build script changes

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
+
+[ -f build-hook.sh ] && ./build-hook.sh
+
 #deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
 #notify-send 'rebuild.sh requires auth'
 #sudo nix-env -p /nix/var/nix/profiles/system --set $deriv
 #sudo $deriv/bin/switch-to-configuration switch
 
-NIXPKGS_ALLOW_UNFREE=1 nh os switch . -- --impure
+NIXPKGS_ALLOW_UNFREE=1 nh os switch . -- --impure -v

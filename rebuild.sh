be715322601687bfbc2448e4175206744dcf8a07
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-02-17 20:40:11 +0300

    rebuild script sudo check

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,8 @@
 set -e
 cd "$(dirname "$0")"
 
+sudo echo "sudo test OK" || (echo "sudo test FAIL; rerun this outside the FHS env" && exit 1)
+
 [ -f build-hook.sh ] && ./build-hook.sh
 
 #deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
@@ -9,4 +11,4 @@ cd "$(dirname "$0")"
 #sudo nix-env -p /nix/var/nix/profiles/system --set $deriv
 #sudo $deriv/bin/switch-to-configuration switch
 
-NIXPKGS_ALLOW_UNFREE=1 nh os switch . -- --impure -v
+NIXPKGS_ALLOW_UNFREE=1 nh os switch . -- --impure -v --show-trace
